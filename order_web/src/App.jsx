2aa6254a57ce4ce3cd7523277696dd54de496170
haidangez <haidangbay2003@gmail.com>
2025-06-29 11:46:06 +0700

    update

--- order_web/src/App.jsx
+++ order_web/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { HomePage } from "./screen/HomePage";
 import { PlaceScreen } from "./screen/PlaceScreen";
+import { Parent } from "./screen/Parent";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage/>}/>
         <Route path="/placeorder" element={<PlaceScreen/>} />
+        <Route path="/test" element={<Parent/>} />
       </Routes>
     </Router>
   );
